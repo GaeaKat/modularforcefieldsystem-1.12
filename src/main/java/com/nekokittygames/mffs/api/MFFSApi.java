ce157c06af407a3880870bfdfda7aea0f8df5836
nekosune <kat.swales@nekokittygames.com>
2016-04-03 17:00:30 +0100

    Added default artifacts

---
+++ src/main/java/com/nekokittygames/mffs/api/MFFSApi.java
@@ -0,0 +1,9 @@
+package com.nekokittygames.mffs.api;
+
+/**
+ * Created by katsw on 03/04/2016.
+ */
+public class MFFSApi {
+
+    public static final String Version="1.0";
+}
---
+++ src/main/java/com/nekokittygames/mffs/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by katsw on 03/04/2016.
+ */
+@API(apiVersion = MFFSApi.Version, owner = "mffs", provides = "mffsAPI")
+package com.nekokittygames.mffs.api;
+
+
+import net.minecraftforge.fml.common.API;
